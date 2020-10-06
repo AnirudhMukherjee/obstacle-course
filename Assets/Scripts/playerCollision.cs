ca4c652a560a95efc0e4ce6452b1bfbc6e14ae7d
AnirudhMukherjee <anirudhmk130@gmail.com>
2020-10-06 20:34:32 +0530

    init

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+	public playerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo){
+    	if(collisionInfo.collider.tag == "Obstacle"){
+    		GetComponent<playerMovement>().enabled = false;
+    		FindObjectOfType<GameManager>().EndGame();
+    	}
+    }
+}
